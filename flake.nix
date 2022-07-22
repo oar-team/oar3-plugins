a2e5249df38e82cfbd0bac4aad7d9a868db8517c
Adrien Faure <adrien.faure@protonmail.com>
2022-07-22 10:14:41 +0200

    flake: format flake

--- flake.nix
+++ flake.nix
@@ -6,47 +6,51 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        app = pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          propagatedBuildInputs = [ ];
-          editablePackageSources = {
-             oar-plugins = ./src;
-          };
+      app = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+        propagatedBuildInputs = [];
+        editablePackageSources = {
+          oar-plugins = ./src;
         };
-         overrides_oar = pkgs.poetry2nix.defaultPoetryOverrides.extend ( self: super: {
-            oar = super.oar.overridePythonAttrs (
-              old: {
-                buildInputs = (old.buildInputs or [ ]) ++ [ self.poetry ];
-              }
-            );
-          });
-        packageName = "oar-plugins";
-      in {
-        packages.${packageName} = app;
+      };
+      # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-poetry
+      overrides_oar = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super: {
+        oar = super.oar.overridePythonAttrs (
+          old: {
+            buildInputs = (old.buildInputs or []) ++ [self.poetry];
+          }
+        );
+      });
+      packageName = "oar-plugins";
+    in {
+      packages.${packageName} = app;
 
-        defaultPackage = self.packages.${system}.${packageName};
+      defaultPackage = self.packages.${system}.${packageName};
 
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (poetry2nix.mkPoetryEnv {
-              projectDir = self;
-              overrides = overrides_oar;
-            })
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (poetry2nix.mkPoetryEnv {
+            projectDir = self;
+            overrides = overrides_oar;
+          })
 
-            (poetry2nix.mkPoetryApplication {
-              projectDir = self;
-              overrides = overrides_oar;
-            })
-            poetry
-            postgresql
-            pre-commit
-          ];
-        };
+          (poetry2nix.mkPoetryApplication {
+            projectDir = self;
+            overrides = overrides_oar;
+          })
+          poetry
+          postgresql
+          pre-commit
+        ];
+      };
+      formatter = nixpkgs.legacyPackages.x86_64-linux.alejandra;
     });
 }
