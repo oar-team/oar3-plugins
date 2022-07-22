0b8b8c0302e36d62423d0cb7115575ec84c27743
Adrien Faure <adrien.faure@protonmail.com>
2022-07-22 11:50:26 +0200

    test: run tests with docker and postgres

--- scripts/ci/docker/build-venv.sh
+++ scripts/ci/docker/build-venv.sh
@@ -8,6 +8,10 @@ pip install -U pip
 pip install wheel
 pip --no-cache-dir install poetry
 
+ls -l /app
+# So poetry would build
+# rsync -r . /app
+
 # Project initialization:
 cd app && POETRY_VIRTUALENVS_CREATE=false poetry install
 
